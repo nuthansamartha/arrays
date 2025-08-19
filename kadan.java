37c2b31d8008a60bca75fb6519e35bb89407a273
nuthansamarth <2024cs_nuthansamarthah_c@nie.ac.in>
2025-08-19 19:17:24 +0530

    both positive and all negative elements

--- kadan.java
+++ kadan.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 public class kadan {
     public static void kadan_sum(int arr[]) {
+        int n=arr.length;
         int cs=0;
         int ms=Integer.MIN_VALUE;
+        
+        
+        int flag=0;
         for(int i=0;i<arr.length;i++){
             cs=cs+arr[i];
             if (cs<0){
@@ -10,15 +14,45 @@ public class kadan {
             }
             if(cs>ms){
                 ms=cs;
-            }   
+            }
+            
         }
-        System.out.println(ms);
-        System.out.println(Integer.MAX_VALUE);       
-        System.out.println(Integer.MIN_VALUE);       
+        
+        if(cs==0){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n-i-1;j++){
+                    if(arr[j]>arr[j+1]){
+                        int temp=arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=temp;
+
+
+                    }
+                }
+                
+            }
+            System.out.println("the largest element among all the neagtive elements in the array is : "+arr[n-1]);
+            flag=1;
+        }
+
+        
+        
+        if(flag!=1){
+            System.out.println(ms);
+
+        }
+       
+          
     }
     public static void main(String[] args) {
+        System.out.println("enter the length of the array: ");
         Scanner scn=new Scanner(System.in);
-        int arr[]={-1,-2,-7,10,1};
+        int num=scn.nextInt();
+        int arr[]=new int[num];
+        System.out.println("enter the elements of the array: ");
+        for(int i=0;i<num;i++){
+            arr[i]=scn.nextInt();
+        }
         kadan_sum(arr);
         scn.close();
         
